565116016dbeb2c46efd1a7f9339a16a1dd736f4
DrDonoso <davidrodriguezdonoso95@gmail.com>
2021-02-19 19:31:54 +0100

    Modify docker-compose and dockerfiles

--- Boilerplate/build.sh
+++ Boilerplate/build.sh
@@ -6,7 +6,8 @@ docker-compose -f docker-compose.tests.yml build
 
 docker-compose -f docker-compose.override.yml up --force-recreate -d
 
-docker-compose -f docker-compose.tests.yml run boilerplate-api-integrationtests
-docker-compose -f docker-compose.tests.yml run boilerplate-api-unittests
+set +e
+docker-compose -f docker-compose.tests.yml run --rm boilerplate-api-integrationtests
+docker-compose -f docker-compose.tests.yml run --rm boilerplate-api-unittests
 
 docker-compose -f docker-compose.override.yml stop
